02340be748adde01e9d2d2aeee998d59a0cb5f00
hunkel <kolan19988@yandex.ru>
2019-09-24 15:12:13 +0500

    add db auth

--- WSDExample/UI/AuthWindow.xaml.cs
+++ WSDExample/UI/AuthWindow.xaml.cs
@@ -1,12 +1,31 @@
+using System;
+using System.Data.SqlClient;
 using System.Windows;
+using WSDExample.Classes.Database;
 
 namespace WSDExample
 {
     public partial class AuthWindow : Window
     {
+        private DatabaseManager dbManager;
         public AuthWindow()
         {
             InitializeComponent();
+            dbManager = new DatabaseManager("DESKTOP-OO2V3AM", "usersdb");
         }
+
+        public void LoginButton_Click(object sender, RoutedEventArgs routedEventArgs)
+        {
+            var res = dbManager.Auth(LoginTextBox.Text.ToString(), PasswordTextBox.Password.ToString());
+            MessageBox.Show(res.ToString(), "Success");
+
+        }
+
+        public void RegisterButton_Click(object sender, RoutedEventArgs routedEventArgs)
+        {
+            
+        }
+        
+        
     }
 }
