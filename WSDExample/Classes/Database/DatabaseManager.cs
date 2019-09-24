d3f44fa1e0c4f24bcfec998a9a2dd7343b6a69fb
hunkel <kolan19988@yandex.ru>
2019-09-24 13:48:00 +0500

    init files

---
+++ WSDExample/Classes/Database/DatabaseManager.cs
@@ -0,0 +1,72 @@
+using System;
+using System.Data;
+using System.Data.SqlClient;
+using System.Text;
+using System.Threading;
+using System.Windows;
+
+namespace WSDExample.Classes.Database
+{
+    public class DatabaseManager
+    {
+        private string dataSource = "";
+        private string database = "";
+        private string table = "";
+        private string login = "";
+        private string password = "";
+        
+        private string BASE_DATABASE_URL =
+            "Data Source={0};Initial Catalog={1};Integrated Security={2};Trusted_Connection=Yes;DataBase={3};";
+
+        private string USER_AUTH =
+            "User ID={0};Password={1};";
+
+        private string connectionUri = "";
+
+        private bool connected = false;
+        
+        public DatabaseManager(string _dataSource, string _database, string _table)
+        {
+            dataSource = _dataSource;
+            database = _database;
+            connectionUri = String.Format(BASE_DATABASE_URL, dataSource, table, true, database);
+            connect();
+        }
+        
+        public DatabaseManager(string _dataSource, string _database, string _table, string _login, string _password)
+        {
+            dataSource = _dataSource;
+            database = _database;
+            connectionUri = String.Format(BASE_DATABASE_URL, dataSource, table, true, database);
+            connectionUri += ";" + String.Format(USER_AUTH, _login, _password);
+            connect();
+        }
+
+
+        private void connect()
+        {
+            SqlConnection connection = new SqlConnection(connectionUri);
+            try
+            {
+                connection.Open();
+                connected = true;
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show("Connection error", "Error");
+            }
+        }
+        
+        public DataTable authInDb(string selectSQL)
+        {
+            DataTable dataTable = new DataTable("dataBase");
+            SqlConnection connection = new SqlConnection("");
+            connection.Open();
+            SqlCommand sql = connection.CreateCommand();
+            sql.CommandText = selectSQL;
+            SqlDataAdapter sqlDataAdapter = new SqlDataAdapter(sql);
+            sqlDataAdapter.Fill(dataTable);
+            return dataTable;
+        }
+    }
+}
---
+++ WSDExample/UI/AuthWindow.xaml.cs
@@ -0,0 +1,12 @@
+using System.Windows;
+
+namespace WSDExample
+{
+    public partial class AuthWindow : Window
+    {
+        public AuthWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
