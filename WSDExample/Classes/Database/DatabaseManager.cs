5ee2c95efbe9ddda52d55db7f8bf74f02e89f318
hunkel <kolan19988@yandex.ru>
2019-09-24 15:50:27 +0500

    refactor base & cleanup

--- WSDExample/Classes/Database/DatabaseManager.cs
+++ WSDExample/Classes/Database/DatabaseManager.cs
@@ -11,16 +11,10 @@ namespace WSDExample.Classes.Database
     {
         private string dataSource = "";
         private string database = "";
-        private string table = "";
-        private string login = "";
-        private string password = "";
         
         private string BASE_DATABASE_URL =
             "Data Source={0};Integrated Security={1};Trusted_Connection=Yes;Initial Catalog = {2}";
-
-        private string USER_AUTH =
-            "User ID={0};Password={1};";
-
+        
         private string connectionUri = "";
 
         private bool connected = false;
@@ -35,36 +29,23 @@ namespace WSDExample.Classes.Database
             Connect();
         }
         
-        public DatabaseManager(string _dataSource, string _database, string _table, string _login, string _password)
-        {
-            dataSource = _dataSource;
-            database = _database;
-            connectionUri = String.Format(BASE_DATABASE_URL, dataSource, table, true, database);
-            connectionUri += ";" + String.Format(USER_AUTH, _login, _password);
-            Connect();
-        }
-
         public bool Auth(string login, string password)
         {
-            var command = new SqlCommand
-            {
-                CommandText = $"SELECT * FROM Users WHERE login = '{login}' AND password = '{password}'"
-            };
-            command.Connection = connection;
             var result = false;
-            try
+            if (connected)
             {
-                using (var reader = command.ExecuteReader())
+
+                var command = new SqlCommand(
+                    $"SELECT * FROM Users WHERE login = '{login}' AND password = '{password}'",
+                    connection);
+                try
                 {
+                    var reader = command.ExecuteReader();
                     result = reader.Read();
                     reader.Close();
                 }
+                catch (Exception e) {}
             }
-            catch (Exception e)
-            {
-                result = false;
-            }
-            
             return result;
         }
 
